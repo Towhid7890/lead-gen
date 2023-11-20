d6140616954a208dcebd79a3dc1066447d99ba6e
thohidul.islam <thohidul.islam@trainingmug.com>
2023-11-20 17:25:04 +0600

    add

--- app/components/KeyfobsPrice.jsx
+++ app/components/KeyfobsPrice.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 
 const KeyfobsPrice = () => {
-  const [customers, setCustomers] = useState(0);
+  const [customers, setCustomers] = useState(250);
   const [price, setPrice] = useState(0.39);
   const [bagsRings, setBagsrings] = useState(0);
   const [optionsyes, setOptionsyes] = useState("Yes");
@@ -14,6 +14,9 @@ const KeyfobsPrice = () => {
     setOptionsyes(e.target.value);
   };
 
+  const handleRadioChange = (value) => {
+    setCustomers(value);
+  };
   const handleChange = (e) => {
     setCustomers(e.target.value);
   };
@@ -148,6 +151,26 @@ const KeyfobsPrice = () => {
           type="text"
           className="w-full px-2 py-2 mb-4 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
         />
+        <div className="flex justify-center items-center gap-4 fontGeneral py-2 border-2 mb-3 w-[60%] rounded-lg md:justify-around">
+          <label className="text-[1.5rem] font-bold">
+            <input
+              onClick={() => handleRadioChange(250)}
+              type="radio"
+              name="optionsyes"
+              value="Yes"
+            />{" "}
+            Yes
+          </label>
+          <label className="text-[1.5rem] font-bold">
+            <input
+            onClick={() => handleRadioChange(0)}
+              type="radio"
+              name="optionsyes"
+              value="No"
+            />{" "}
+            No
+          </label>
+        </div>
         <p className="text-red-700 text-[1.5rem]" style={{ fontWeight: "700" }}>
           50% DISCOUNT TODAY!
         </p>
